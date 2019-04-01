3cd85686625744aaa9d4d53b7afa4164b92f0cc2
Lina Mezghani <35572460+LinaMezghani@users.noreply.github.com>
2019-04-01 22:22:30 +0100

    Adding generation of the submission file

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import random
+from SpectrumKernel import SpectrumKernel
+import csv
 
 def convert_prediction(pred):
     if pred == -1 :
@@ -13,3 +15,37 @@ def compute_val_accuracy(classifier, X_val, labels_val):
         if convert_prediction(classifier.predict(X_val[i])) == labels_val[i]:
             acc += 1
     return acc/X_val.shape[0]
+
+def to_vect_test(datafile, numbers_list, dictionnary=True):
+    # Takes a dataframe, returns the corresponding dataframe containing :
+    # The dictionaries of k-grams corresponding to each line if dictionnary = True
+    # Vectors corresponding to the dictionnaries if dictionnary = False
+    kernel = SpectrumKernel(datafile, numbers_list)
+    data = kernel.special_vectorization()
+    return data
+
+
+def generate_predictions(test_file, path, numbers_list, classifier = False):
+    test_data = to_vect_test(path + test_file,numbers_list)
+    predictions = []
+    for i in range(len(test_data)):
+        emb_seq = test_data[i]
+        prediction = (classifier.predict(emb_seq))
+        prediction = convert_prediction(prediction)
+        predictions.append(int(prediction))
+    return predictions
+
+
+
+def generate_submission_file(classifiers, path, numbers_list, submission_filename = 'submission.csv'):
+    test_files = ['Xte0.csv', 'Xte1.csv', 'Xte2.csv']
+    predictions = []
+    for i in range(len(test_files)) :
+        predictions += generate_predictions(test_files[i], path, numbers_list, classifiers[i])
+    i = 0
+    with open(submission_filename, mode='w') as submission_file:
+        writer = csv.writer(submission_file, delimiter=',')
+        writer.writerow(['Id', 'Bound'])
+        for prediction in predictions :
+            writer.writerow([str(i), prediction])
+            i += 1
